e8723cbced03d6db6e7b64446aedd963cf1b6592
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-11-25 13:59:12 +0100

    Add files via upload

---
+++ arr_01.js
@@ -0,0 +1,75 @@
+
+
+/*
+Aufgabe:
+<html><head></head><body><h1></h1><p></p></body></html>
+
+	---->
+
+<html>
+	<head>
+	</head>
+	<body>
+		<h1></h1>
+		<p></p>
+	</body>
+</html>
+
+--> return : "\n"  
+--> Tab: "\t"
+
+
+Verwenden Sie dafür die untenstehenden Arrays
+*/
+
+const controls = ["<", "</", ">"];
+const tags = ["html","head","head","body","h1","h1","p","p","body","html"];
+let stack = [];
+
+output(getHTML());
+function getHTML() {
+
+    let htmlStr = "";
+
+    for (let i = 0; i < tags.length; i++) {
+        if (isOpenTag(tags[i])) { 
+            htmlStr += getTags(tags[i],"open"); 
+        } else {
+            htmlStr += getTags(tags[i],"close");
+        }
+    }
+
+    return htmlStr;
+}
+
+// Modul: open || close ?
+function isOpenTag(tag) {
+   
+     const cond = (tag != stack[stack.length -1]); // tag liegt oben!
+
+    if (cond) {
+        stack.push(tag);
+        return true;
+    } else {
+        stack.pop();
+        return false;
+    }
+}
+
+// Modul: Zusammenbau: <tagStr> --> Tests:
+function getTags(tag,op) {
+    switch (op) {
+        case "open":
+            return controls[0] + tag + controls[2];
+        case "close":
+            return controls[1] + tag + controls[2];
+        default:
+            return "#!";
+    } 
+}
+
+// Modul: Ausgabe | Test
+//output("hi");
+function output(outputData) {
+    console.log(outputData);
+}
